13a5dbf491eb83bfdfc7963533d437bfe31f930a
Hesslor <102875186+Hesslor@users.noreply.github.com>
2024-05-08 00:59:48 +0530

    initial

---
+++ panni.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const panni = () => {
+  return (
+    <View>
+      <Text>panni</Text>
+    </View>
+  )
+}
+
+export default panni
+
+const styles = StyleSheet.create({})
